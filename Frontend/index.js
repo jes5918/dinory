dff11529563fe1283fe06a62346c1b53116d515b
신민호(본인의 이름) <eeqwer123@gmail.com()>
2021-03-11 21:48:19 +0900

    [refactor/FE] : 프로젝트 구조 설계
    Former-commit-id: 9f46092ad244cc6c47ba364646260d85949d597a

--- Frontend/index.js
+++ Frontend/index.js
@@ -3,7 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/container/App';
 import {name as appName} from './app.json';
-
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
 AppRegistry.registerComponent(appName, () => App);
