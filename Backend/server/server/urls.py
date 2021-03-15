8e6650e3366f22e9c6b395020db72324c9591186
zngsm <68373473+zngsm@users.noreply.github.com>
2021-03-15 22:16:51 +0900

    [fix] nginx error 디버깅
    - app 임시 주석

--- Backend/server/server/urls.py
+++ Backend/server/server/urls.py
@@ -20,12 +20,12 @@ from .yasg import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('Accounts.urls')),
-    path('notes/', include('Notes.urls')),
+    # path('accounts/', include('Accounts.urls')),
+    # path('notes/', include('Notes.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += [
-        re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name="schema-json"),
-        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),    ]
+# if settings.DEBUG:
+#     urlpatterns += [
+#         re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name="schema-json"),
+#         re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+#         re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),    ]
