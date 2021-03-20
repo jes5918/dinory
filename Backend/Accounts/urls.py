cd34566b50844faae34971002ac28bf7a564dae7
zngsm <68373473+zngsm@users.noreply.github.com>
2021-03-20 23:43:37 +0900

    [feat] 이메일 인증
    - /accounts/email/
    - 이메일 form 전송시 -> 해당 이메일로 인증코드 전송
    - 인증코드 클라이언트에 응답
    
    
    Former-commit-id: 8a6de155313e1a73a90545f3d8d2ac78abe5bc88

--- Backend/Accounts/urls.py
+++ Backend/Accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
+    path('email/', views.email_check),
     path('signup/', views.signup),
     path('login/', obtain_jwt_token),
     path('child/', views.register_child),
