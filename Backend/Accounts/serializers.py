730d4d7734a558d067ef68a8dab56ca1c6104ba3
zngsm <68373473+zngsm@users.noreply.github.com>
2021-03-21 01:46:51 +0900

    [feat/BE] 유저(관리자) 로직 완성
    - 회원가입
    - 로그인
    - 비밀번호 변경
    - 핀번로 변경
    - 아이디 중복조회
    - 이메일 인증
    - 회원 탈퇴
    
    
    Former-commit-id: ab6a4fc244e974a6264f1416dd681928184aacf3 [formerly c8544b84d07222635d6dcf853b5efc3987766569]
    Former-commit-id: ec3e518833588ced3d72e8a4af1a14f3c3d871ac

--- Backend/Accounts/serializers.py
+++ Backend/Accounts/serializers.py
@@ -1,13 +1,34 @@
 from rest_framework import serializers
-
+from django.contrib.auth.models import update_last_login
+from django.contrib.auth import authenticate
 from django.contrib.auth import get_user_model
+
 from .models import Child
+# from rest_framework_jwt.settings import api_settings
+# JWT_PAYLOAD_HANDLER = api_settings.JWT_PAYLOAD_HANDLER
+# JWT_ENCODE_HANDLER = api_settings.JWT_ENCODE_HANDLER
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ('username', 'password', 'email', 'pin_code')
+
+
+class UserPasswordChangeSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields = ('password', )
+
+
+class UserPincodeChangeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('pin_code', )
 
 class ChildSerializer(serializers.ModelSerializer):
     class Meta:
