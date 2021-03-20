eb1879d1177e53e28f543f4a8a8697855ef3d2e4
zngsm <68373473+zngsm@users.noreply.github.com>
2021-03-21 01:46:51 +0900

    [feat/BE] 유저(관리자) 로직 완성
    - 회원가입
    - 로그인
    - 비밀번호 변경
    - 핀번로 변경
    - 아이디 중복조회
    - 이메일 인증
    - 회원 탈퇴
    
    
    Former-commit-id: 2dbde3a29d51da3195db51bacd06fccfeca3ae08

--- Backend/Accounts/urls.py
+++ Backend/Accounts/urls.py
@@ -4,8 +4,15 @@ from . import views
 from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
-    path('email/', views.email_check),
+    # 관리자 계정
     path('signup/', views.signup),
     path('login/', obtain_jwt_token),
-    path('child/', views.register_child),
+    path('check/email/', views.email_check),
+    path('check/user/', views.user_check),
+    path('setting/password/<int:pk>/', views.password_setting),
+    path('setting/pincode/<int:pk>/', views.pincode_setting),
+    path('setting/delete/<int:pk>/', views.user_delete),
+
+    # 아이계정
+    path('child/', views.child_create_read),
 ]
