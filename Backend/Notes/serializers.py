8d0399acb78ba0897fcfb69ecad5feef57742323
zngsm <68373473+zngsm@users.noreply.github.com>
2021-03-21 23:47:01 +0900

    [feat/BE] 노트
    Former-commit-id: a5f42b2cceb70bb7e04ec5b7c4abc7ef0409cc6b [formerly aeab536a41a3fc3cab3b466ea99bf95d7b86100c]
    Former-commit-id: 6b5a0bcb79d099ac2c00256e42fec65ba0b8312c

---
+++ Backend/Notes/serializers.py
@@ -0,0 +1,25 @@
+# from django.db.models import fields
+# from rest_framework import serializers
+
+# from .models import Diary, Sentence, Word
+
+# class DiarySerializer(serializers.ModelSerializer):
+
+#     class Meta:
+#         model = Diary
+#         fields = '__all__'
+#         read_only_fields = ('user',)
+
+# class SentenceSerializer(serializers.ModelSerializer):
+
+#     class Meta:
+#         model = Sentence
+#         fields = '__all__'
+#         read_only_fields = ('diary', 'user',)
+
+# class WordSerializer(serializers.ModelSerializer):
+
+#     class Meta:
+#         model = Word
+#         fields = '__all__'
+#         read_only_fields = ('user',)
