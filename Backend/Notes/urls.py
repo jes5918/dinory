f191f804824c60347c03dd1fc46d7ba6e3d6d8fc
zngsm <68373473+zngsm@users.noreply.github.com>
2021-03-22 11:06:45 +0900

    [feat/BE] Note 작성
    - 노트작성 진행중
    - ai 삭제
    
    
    Former-commit-id: 7df3df4c0ac977bddc3f17506e596341a6af1281

--- Backend/Notes/urls.py
+++ Backend/Notes/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    # path('diary/', views.diarylist_create),
-    # path('word/', views.word_read_craete),
+    path('diary/', views.diarylist_create),
+    path('word/', views.word_read_craete),
 ]
