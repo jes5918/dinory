2abf3744ed0ed38f12e778ae16d09508b60752cf
신민호(본인의 이름) <eeqwer123@gmail.com()>
2021-03-22 23:41:20 +0900

    [feat/FE] : layout컴포넌트 개발
    배경은 각자 설정해야해요~  width와 height, opacity로 입력받습니다~
    
    
    Former-commit-id: 18b0fe0e206199035dd937a578c45b16863aa653

--- Frontend/src/components/elements/layout.js
+++ Frontend/src/components/elements/layout.js
@@ -1,42 +1,37 @@
-import React from "react";
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import {ImageBackground, StyleSheet, Text, View} from 'react-native';
 
-const backgroundimage = require('../../assets/images/background1.png'); // 배경사진 설정!
-
-const Layout = () => (
-  <View style={styles.container}>
-    <ImageBackground source={backgroundimage} style={styles.backgroundimage}>
-      <View style={styles.layout}>
-        {/* 개발 시작 부분 */}
-        {/* <Text>잘나옵니까?</Text>   */}
-        </View>
-    </ImageBackground>
-  </View>
-);
+export default function Layout({width, height, opacity, children}) {
+  //예시 <Layout width={1000} height={200} opacity={0.5}>
+  // const backgroundImageURL = `../../assets/images/background1.png`;
+  // const backgroundImage = require(backgroundImageURL); // 배경사진 설정하세요!
+  return (
+    <View style={styles.container}>
+      <View
+        style={[
+          styles.layout,
+          {width, height, backgroundColor: `rgba(255,255,255,${opacity})`},
+        ]}>
+        {children}
+      </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column"
-  },
-  backgroundimage: {
-    flex: 1,
-    resizeMode: "cover", 
+    resizeMode: 'cover',
     // resizeMode ==이미지를 꽉 채워줌
-    justifyContent: "center",
-    alignItems:'center'
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   layout: {
     // Customizing 부분
-    width:1000,
-    height:500,
-    opacity:0.7,
-    padding:50,
-    justifyContent:'center',
-    alignItems:'center',
-    backgroundColor: "#f8f8ff",
-    borderRadius:50
-  }
+    padding: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f8f8ff',
+    borderRadius: 50,
+  },
 });
-
-export default Layout;
