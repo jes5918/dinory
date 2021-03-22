0089c898c1898ec09ea7b90efecb461809bd5737
zngsm <68373473+zngsm@users.noreply.github.com>
2021-03-22 22:07:41 +0900

    [feat/BE] setting 수정
    Former-commit-id: f84d7c52953ef838f3c4bfc61990d1865a5a963c

--- Backend/Notes/serializers.py
+++ Backend/Notes/serializers.py
@@ -1,25 +1,33 @@
 from django.db.models import fields
 from rest_framework import serializers
 
-from .models import Diary, Sentence, Word
+from .models import Note, Diary, Sentence
+
+class NoteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Note
+        fields = '__all__'
+        read_only_fields = ('user', 'child')
+        # depth = 1
 
 class DiarySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Diary
         fields = '__all__'
-        read_only_fields = ('user',)
+        read_only_fields = ('note',)
 
 class SentenceSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Sentence
         fields = '__all__'
-        read_only_fields = ('diary', 'user',)
+        read_only_fields = ('user', 'child', )
 
-class WordSerializer(serializers.ModelSerializer):
+# class WordSerializer(serializers.ModelSerializer):
 
-    class Meta:
-        model = Word
-        fields = '__all__'
-        read_only_fields = ('user',)
+#     class Meta:
+#         model = Word
+#         fields = '__all__'
+#         read_only_fields = ('user',)
