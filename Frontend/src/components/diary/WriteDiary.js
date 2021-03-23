cdcaedff4d4fac6eb2c855e2d9854ef4d5c156d7
명도균 <dorosee97@gmail.com>
2021-03-23 16:53:52 +0900

    [feat/FE] : write diary component 제작
    - 글 보여지는 곳
    - grammar check 버튼
    - 단어 리스트
    - ocr 테스
    
    Resolves : #resolve S04P23B105-130
    
    
    Former-commit-id: df05e5aa5c6d414241484a698462a460ff59c933 [formerly ab6efa19224f0856d68040af5499e78e48e13f3f]
    Former-commit-id: 95a4ac9a2967a831eed3603cf85ffd6c48d4d0cf

--- Frontend/src/components/diary/WriteDiary.js
+++ Frontend/src/components/diary/WriteDiary.js
@@ -6,25 +6,47 @@ import {
   StyleSheet,
   TextInput,
   Platform,
+  Button,
+  TouchableOpacity,
+  ImageBackground,
 } from 'react-native';
 
 import CheckBox from '../elements/CheckBox.js';
 
-export default function WriteDiary() {
-  const words = [
+export default function WriteDiary({words}) {
+  const temp = [
     {textEn: 'happy', textKr: '행복'},
     {textEn: 'coding', textKr: '코딩'},
     {textEn: 'people', textKr: '사람'},
     {textEn: 'computer', textKr: '컴퓨터'},
+    {textEn: 'computer', textKr: '컴퓨터'},
+    {textEn: 'computer', textKr: '컴퓨터'},
+    {textEn: 'computer', textKr: '컴퓨터'},
+    {textEn: 'computer', textKr: '컴퓨터'},
   ];
+  const grammarCheck = () => {
+    alert('Grammar Check!');
+  };
 
   return (
     <KeyboardAvoidingView behavior={'heigth'} style={[styles.container]}>
       <View style={[styles.dirayBox]}>
-        <TextInput></TextInput>
+        <TextInput
+          style={[styles.textInput]}
+          multiline
+          autoCompleteType={'off'}
+          autoFocus
+          defaultValue={'제목 : '}></TextInput>
+        <View style={[styles.grammarCheckBtnBox]}>
+          <TouchableOpacity
+            onPress={() => grammarCheck()}
+            style={[styles.grammarCheckBtn]}>
+            <Text style={[styles.btnText]}>문법체크</Text>
+          </TouchableOpacity>
+        </View>
       </View>
       <View style={[styles.wordListBox]}>
-        {words.map((word, i) => {
+        {temp.map((word, i) => {
           return (
             <CheckBox
               textEn={word.textEn}
@@ -38,28 +60,75 @@ export default function WriteDiary() {
 }
 
 const styles = StyleSheet.create({
+  textInput: {
+    // borderWidth: 2,
+    width: '85%',
+    fontFamily: 'HoonPinkpungchaR',
+    fontSize: 24,
+    borderRadius: 30,
+    borderColor: 'gray',
+  },
   container: {
     display: 'flex',
     flexDirection: 'column',
-    justifyContent: 'space-around',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    padding: '2%',
+    padding: '1.5%',
   },
   dirayBox: {
     width: '70%',
-    height: '50%',
+    height: '55%',
     backgroundColor: '#FFF',
     borderRadius: 30,
-  },
-  wordListBox: {
-    width: '70%',
-    height: '40%',
-    backgroundColor: '#FFF',
-    borderRadius: 30,
+    padding: '1.5%',
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    padding: '2%',
+  },
+  wordListBox: {
+    width: '70%',
+    height: 'auto',
+    backgroundColor: '#FFF',
+    borderRadius: 30,
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '1.5%',
+  },
+  grammarCheckBtnBox: {
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 2,
+      height: 1,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3,
+    elevation: 5,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  grammarCheckBtn: {
+    width: 80,
+    height: 80,
+    borderRadius: 50,
+    backgroundColor: '#FB537B',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnText: {
+    fontFamily: 'HoonPinkpungchaR',
+    width: '50%',
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 24,
   },
 });
