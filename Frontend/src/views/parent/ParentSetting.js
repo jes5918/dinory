e9d5c44ef99c06fc942f1a91e70ad9fdaffdcf93
유진우 <phoenix9373@naver.com>
2021-03-23 13:45:59 +0900

    [feat/FE] : Word Component 완료
    Resolves : S04P23B105-21 #resolve
    
    리베이스할 커밋
    
    
    Former-commit-id: 14c64ed0a373dfd005c81e3eed1189ba41c7a9aa

---
+++ Frontend/src/views/parent/ParentSetting.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import CheckBox from '../../components/elements/CheckBox';
+
+function ParentSetting(props) {
+  return (
+    <View>
+      <CheckBox></CheckBox>
+    </View>
+  );
+}
+
+export default ParentSetting;
