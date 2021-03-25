97a567cf0849b4f9ef8df118881c6571f59054bb
신민호 <eeqwer123@gmail.com>
2021-03-25 05:33:59 +0000

    Feat/FE Authorization Css Done
    Former-commit-id: ae227b20edfe1374bab9d4da49b858be2f28f600

--- Frontend/src/components/elements/layout.js
+++ Frontend/src/components/elements/layout.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import {ImageBackground, StyleSheet, Text, View} from 'react-native';
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+} from 'react-native';
 
 export default function Layout({width, height, opacity, children}) {
-  //예시 <Layout width={1000} height={200} opacity={0.5}>
-  // const backgroundImageURL = `../../assets/images/background1.png`;
-  // const backgroundImage = require(backgroundImageURL); // 배경사진 설정하세요!
   return (
     <View style={styles.container}>
       <View
         style={[
           styles.layout,
-          {width, height, backgroundColor: `rgba(255,255,255,${opacity})`},
+          {
+            width: width || 'auto',
+            height: height || 'auto',
+            backgroundColor: `rgba(255,255,255,${opacity})`,
+          },
         ]}>
         {children}
       </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -27,11 +33,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   layout: {
-    // Customizing 부분
-    padding: 50,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f8f8ff',
-    borderRadius: 50,
+    borderRadius: 30,
   },
 });
