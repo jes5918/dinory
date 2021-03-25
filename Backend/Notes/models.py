ae1e65b3b232da06c974887c9f4d22712017eebd
zngsm <68373473+zngsm@users.noreply.github.com>
2021-03-25 19:28:02 +0900

    [feat/BE] : 이미지업로드
    - 이미지... 등등
    
    
    Former-commit-id: 47b278fa259d788c7fdb84fa379b6a2e229307af [formerly 501c344bce88dda98eb95c38ee083c89653a67e9]
    Former-commit-id: f8956e0a61c9fcc72ddc86810678383e739c85a5

--- Backend/Notes/models.py
+++ Backend/Notes/models.py
@@ -6,7 +6,7 @@ class Note(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     child = models.ForeignKey('Accounts.Child', on_delete=models.CASCADE)
     vol = models.IntegerField()
-    img = models.ImageField(upload_to="%Y/%m/%d")
+    img = models.ImageField(upload_to='images/%Y/%m/%d')
     # img = models.CharField(max_length=255)
 
 
@@ -15,7 +15,7 @@ class Diary(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
     # img = models.CharField(max_length=255)
-    img = models.ImageField(upload_to="%Y/%m/%d")
+    img = models.ImageField(upload_to='images/%Y/%m/%d')
     year = models.IntegerField()
     month = models.IntegerField()
     date = models.IntegerField()
@@ -26,6 +26,8 @@ class Word(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     child = models.ForeignKey('Accounts.Child', on_delete=models.CASCADE)
     content = models.CharField(max_length=20)
+    count = models.IntegerField(default=0)
+    state = models.BooleanField()
 
 
 class Sentence(models.Model):
@@ -37,3 +39,4 @@ class Sentence(models.Model):
 class WordToSentence(models.Model):
     sentence = models.ForeignKey('Sentence', on_delete=models.CASCADE)
     word = models.ForeignKey('Word', on_delete=models.CASCADE)
+    
