b53b96f3330f01be2301c8f8cfd6a5a52d86d523
명도균 <dorosee97@gmail.com>
2021-03-27 02:42:52 +0000

    [feat/FE] : grammarCheck 컴포넌트 및 기타 컴포넌트 수정
    Former-commit-id: ea872d2bef0435776ae4e24d39341745b1d1fe80

--- Frontend/src/api/diary/writeDiary.js
+++ Frontend/src/api/diary/writeDiary.js
@@ -9,4 +9,16 @@ function createDiary(diary, child_id, success, fail) {
     .catch(fail);
 }
 
-export {createDiary};
+function imageCaptioning(image, success, fail) {
+  instance.post(`ai/imagecaption/`, image).then(success).catch(fail);
+}
+
+function grammarCheck(sentence, success, fail) {
+  instance.post(`ai/grammarcheck/`, sentence).then(success).catch(fail);
+}
+
+function pronunCheck(speakfile, success, fail) {
+  instance.post(`ai/pronunciation/`, speakfile).then(success).catch(fail);
+}
+
+export {createDiary, imageCaptioning, grammarCheck, pronunCheck};
--- Frontend/src/components/diary/GrammarCheck.js
+++ Frontend/src/components/diary/GrammarCheck.js
@@ -1,10 +1,206 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  StyleSheet,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  ImageBackground,
+  ScrollView,
+} from 'react-native';
+import RoundButton from '../../components/elements/RoundButton';
 
 export default function GrammarCheck() {
+  const grammarCheck = () => {};
+
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <KeyboardAvoidingView behavior={'heigth'} style={[styles.container]}>
+      <View style={[styles.wrapper]}>
+        <View style={[styles.dirayBox]}>
+          <ScrollView style={styles.textInputBox}>
+            <TextInput
+              style={[styles.TitleInput]}
+              autoCompleteType={'off'}
+              autoFocus
+              defaultValue={'제목 : '}></TextInput>
+            <TextInput
+              style={[styles.contentInput]}
+              multiline
+              autoCompleteType={'off'}
+              defaultValue={'alalsek sidji isdjifjsi '}></TextInput>
+            <TextInput
+              style={[styles.contentInput]}
+              multiline
+              autoCompleteType={'off'}
+              defaultValue={'alalsek sidji isdjifjsi '}></TextInput>
+            <TextInput
+              style={[styles.contentInput]}
+              multiline
+              autoCompleteType={'off'}></TextInput>
+            <TextInput
+              style={[styles.contentInput]}
+              multiline
+              autoCompleteType={'off'}
+              defaultValue={'alalsek sidji isdjifjsi '}></TextInput>
+            <TextInput
+              style={[styles.contentInput]}
+              multiline
+              autoCompleteType={'off'}
+              defaultValue={'asdjfejaisdjif ajsidjo eisdij si d'}></TextInput>
+            <TextInput
+              style={[styles.contentInput]}
+              multiline
+              autoCompleteType={'off'}
+              defaultValue={'asdjfejaisdjif ajsidjo eisdij si d'}></TextInput>
+            <TextInput
+              style={[styles.contentInput]}
+              multiline
+              autoCompleteType={'off'}
+              defaultValue={'asdjfejaisdjif ajsidjo eisdij si d'}></TextInput>
+            <TextInput
+              style={[styles.contentInput]}
+              multiline
+              autoCompleteType={'off'}
+              defaultValue={'asdjfejaisdjif ajsidjo eisdij si d'}></TextInput>
+            <TextInput
+              style={[styles.contentInput]}
+              multiline
+              autoCompleteType={'off'}
+              defaultValue={'asdjfejaisdjif ajsidjo eisdij si d'}></TextInput>
+          </ScrollView>
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={() => grammarCheck()}
+          style={{position: 'absolute', right: '6%', top: '15%'}}>
+          <View style={styles.buttonPosition}>
+            <Text style={styles.textIndex}>문</Text>
+            <Text style={styles.textIndex}>법</Text>
+            <Text style={styles.textIndex}>체</Text>
+            <Text style={styles.textIndex}>크</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
+const styles = StyleSheet.create({
+  buttonPosition: {
+    width: 50,
+    height: 150,
+    backgroundColor: '#f0859f',
+    borderTopRightRadius: 30,
+    borderBottomRightRadius: 30,
+    elevation: 5,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textIndex: {
+    fontFamily: 'HoonPinkpungchaR',
+    fontSize: 24,
+    color: '#fff',
+  },
+  TitleInput: {
+    // borderWidth: 2,
+    width: '100%',
+    fontFamily: 'HoonPinkpungchaR',
+    fontSize: 24,
+    borderRadius: 30,
+    borderBottomWidth: 1,
+    borderColor: 'gray',
+    paddingHorizontal: 10,
+  },
+  contentInput: {
+    // borderWidth: 2,
+    width: '100%',
+    fontFamily: 'HoonPinkpungchaR',
+    fontSize: 24,
+    borderRadius: 30,
+    paddingHorizontal: 10,
+    borderBottomWidth: 1,
+    borderColor: 'gray',
+  },
+  textInputBox: {
+    marginHorizontal: 10,
+    paddingHorizontal: 10,
+    // backgroundColor: '#5496',
+  },
+  container: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '1.5%',
+  },
+  dirayBox: {
+    // position: 'relative',
+    width: '80%',
+    backgroundColor: '#FFF',
+    borderRadius: 30,
+    padding: '1.5%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    elevation: 5,
+    marginVertical: 15,
+  },
+  wrapper: {
+    flex: 1,
+    width: '100%',
+    height: '70%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+  },
+  wordListBox: {
+    width: '70%',
+    height: 'auto',
+    backgroundColor: '#FFF',
+    borderRadius: 30,
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '1.5%',
+  },
+  grammarCheckBtnBox: {
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 2,
+      height: 1,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3,
+    elevation: 5,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  grammarCheckBtn: {
+    width: 80,
+    height: 80,
+    borderRadius: 50,
+    backgroundColor: '#FB537B',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnText: {
+    fontFamily: 'HoonPinkpungchaR',
+    width: '50%',
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 24,
+  },
+});
