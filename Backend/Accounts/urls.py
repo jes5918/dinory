543192ca2ebf67684ac6320f497e55f09a1e425d
zngsm <68373473+zngsm@users.noreply.github.com>
2021-03-28 15:37:33 +0900

    [feat/BE] : 계정, 노트, 단어 수정
    - 계정 : pin_code / post시 일치 여부 조회
    - 노트 : child_pk / 쿼리로 보내면 노트 작성 여부확인
    - 단어 : 단어 배열로 여러개 한번에 등록 가능
    
    Resolves : #resolve
    
    
    Former-commit-id: 0b21c57e48c2e254275793b8c8ef083d3b481dd3

--- Backend/Accounts/urls.py
+++ Backend/Accounts/urls.py
@@ -7,12 +7,12 @@ urlpatterns = [
     # 관리자 계정
     path('signup/', views.signup),
     path('login/', obtain_jwt_token),
+    path('check/pin/', views.pin_check),
     path('check/email/', views.email_check),
     path('check/user/', views.user_check),
     path('setting/password/<int:pk>/', views.password_setting),
     path('setting/pincode/<int:pk>/', views.pincode_setting),
     path('setting/delete/<int:pk>/', views.user_delete),
-
     # 아이계정
     path('child/', views.child_create_read),
     path('child/setting/', views.child_update),
