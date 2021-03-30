cb3270815fedaf1ff5a06bbc8a5be929d49c803a
신민호(본인의 이름) <eeqwer123@gmail.com()>
2021-03-30 20:48:52 +0900

    [feat/FE] : 프로필 생성과 프로플 선택 API 통신 완료
    Former-commit-id: 46dbf0e01b2b0e2c05eb7a0e3e9b3b436a478c24

--- Frontend/src/container/App.js
+++ Frontend/src/container/App.js
@@ -43,7 +43,6 @@ import Diary from '../views/diary/Diary';
 import Word from '../views/word/Word';
 import WordByAlphabet from '../views/word/WordByAlphabet';
 import DiaryWriteTutorial from '../views/diary/DiaryWriteTutorial';
-import LoginScreen from '../views/user/LoginScreen';
 import DiaryDetail from '../views/diaryList/DiaryDetail';
 import PassWordUpdate from '../views/parent/PassWordUpdate';
 import PinUpdate from '../views/parent/PinUpdate';
@@ -117,7 +116,6 @@ const App = () => {
         <Stack.Screen name="Word" component={Word} />
         <Stack.Screen name="WordByAlphabet" component={WordByAlphabet} />
         <Stack.Screen name="DiaryDetail" component={DiaryDetail} />
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
         <Stack.Screen
           name="DiaryWriteTutorial "
           component={DiaryWriteTutorial}
