1b886ca54587c11d56b42d88bac91a2804b49737
윤지해 <stanajh@naver.com>
2021-03-30 20:02:35 +0900

    [feat/FE] : 목소리 파일 추가 및 api 연결 출력 목소리 변경을 위한 데이터 추가 및 api 연결, 아이정보 수정 페이지 api 수정
    Former-commit-id: a772bc6cbbc5775f1fadf23b7f94a4fa4b9491e6 [formerly fe67907ddb6e15622e4bdf47a5dd28ffbef8c3d7]
    Former-commit-id: 05b17c60e3e95a9749f4e91994b27be8be52c652

--- Frontend/src/views/child/SelectVoice.js
+++ Frontend/src/views/child/SelectVoice.js
@@ -1,15 +1,88 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, View, Dimensions} from 'react-native';
 import SelectLayout from '../../components/elements/SelectLayout';
 import Header from '../../components/elements/Header';
 import BackgroundAbsolute from '../../components/elements/BackgroundAbsolute';
+import {editChildVoice} from '../../api/accounts/childSettings';
+import Sound from 'react-native-sound';
+import voiceOne from '../../assets/sound/0hellonicetomeetyou.wav';
+import voiceTwo from '../../assets/sound/1hellonicetomeetyou.wav';
+import voiceThr from '../../assets/sound/2hellonicetomeetyou.wav';
+import voiceFou from '../../assets/sound/3hellonicetomeetyou.wav';
+import voiceFiv from '../../assets/sound/4hellonicetomeetyou.wav';
+import AlertModal from '../../components/elements/AlertModal';
+import {useNavigation} from '@react-navigation/core';
 
 const dimensions = Dimensions.get('window');
 const height = dimensions.height;
 
 export default function SelectVoice() {
-  const url = require('../../assets/images/background2.png');
+  const [modalVisible, setModalVisible] = useState(false);
+  const [fmodalVisible, setfModalVisible] = useState(false);
+  const navigation = useNavigation();
 
+  const url = require('../../assets/images/background2.png');
+  let voice = '';
+  const child = '10'; // 임시
+  let soundf = new Sound(voiceOne, Sound.MAIN_BUNDLE, (error) => {
+    if (error) {
+    }
+  });
+  let sounds = new Sound(voiceTwo, Sound.MAIN_BUNDLE, (error) => {
+    if (error) {
+    }
+  });
+  let soundt = new Sound(voiceThr, Sound.MAIN_BUNDLE, (error) => {
+    if (error) {
+    }
+  });
+  let soundfo = new Sound(voiceFou, Sound.MAIN_BUNDLE, (error) => {
+    if (error) {
+    }
+  });
+  let soundfi = new Sound(voiceFiv, Sound.MAIN_BUNDLE, (error) => {
+    if (error) {
+    }
+  });
+  const soundOn = (num) => {
+    console.log(num);
+    voice = num;
+  };
+  const submitVoice = () => {
+    const formData = new FormData();
+    formData.append('voice', voice);
+    editChildVoice(
+      child,
+      formData,
+      (res) => {
+        if (res.status === 200) {
+          changeModalState();
+          setTimeout(() => {
+            navigation.navigate('Main');
+          }, 1500);
+        }
+      },
+      (err) => {
+        console.log(err);
+      },
+    );
+  };
+  const closeModal = () => {
+    setTimeout(() => {
+      setModalVisible(!modalVisible);
+    }, 1000);
+  };
+  const changeModalState = () => {
+    setModalVisible(!modalVisible);
+  };
+  const fcloseModal = () => {
+    setTimeout(() => {
+      setfModalVisible(!fmodalVisible);
+    }, 1000);
+  };
+  const fchangeModalState = () => {
+    setfModalVisible(!fmodalVisible);
+  };
   return (
     <View style={styles.container}>
       <BackgroundAbsolute imageSrc={url}>
@@ -18,6 +91,46 @@ export default function SelectVoice() {
           <SelectLayout
             title={'원하는 목소리를 선택해주세요'}
             btnText={'변경완료'}
+            onHandlePressC1={() => {
+              soundf.setVolume(0.2);
+              soundOn(1);
+              soundf.play();
+            }}
+            onHandlePressC2={() => {
+              sounds.setVolume(0.2);
+              soundOn(2);
+              sounds.play();
+            }}
+            onHandlePressC3={() => {
+              soundt.setVolume(0.2);
+              soundOn(3);
+              soundt.play();
+            }}
+            onHandlePressC4={() => {
+              soundfo.setVolume(0.2);
+              soundOn(4);
+              soundfo.play();
+            }}
+            onHandlePressC5={() => {
+              soundfi.setVolume(0.2);
+              soundOn(5);
+              soundfi.play();
+            }}
+            onHandlePressBasic={() => submitVoice()}
+          />
+          <AlertModal
+            modalVisible={modalVisible}
+            onHandleCloseModal={() => changeModalState()}
+            text={'목소리가 변경되었어요!'}
+            iconName={'smileo'}
+            setTimeFunction={() => closeModal()}
+          />
+          <AlertModal
+            modalVisible={fmodalVisible}
+            onHandleCloseModal={() => fchangeModalState()}
+            text={'다시 시도해주세요!'}
+            iconName={'frowno'}
+            setTimeFunction={() => fcloseModal()}
           />
         </View>
       </BackgroundAbsolute>
