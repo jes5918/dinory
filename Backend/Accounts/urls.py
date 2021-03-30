4396b921066389e05ef497666e14285daed28f72
zngsm <68373473+zngsm@users.noreply.github.com>
2021-03-30 22:37:15 +0900

    [feat/BE] : 계정 api 완성판
    - 이메일 중복조회
    - 사용자 pin번호 해쉬값으로 데이터 저장
    - 이메일 인증시 인증코드 해쉬로 데이터저장
    - 이메일 인증코드 확인 api
    - 비밀번호 찾기시 이메일 인증 api
    - 비밀번호 찾기시 이메일 인증코드 확인 api
    - 비밀번호 찾기시 이메일 정보 수정 api
    
    Resolves : #resolve
    
    
    Former-commit-id: 8317b6acce3b57de153e3a9d972413b2f915ba0b [formerly 8aa6b99108861c545253d88429342dd459a85217]
    Former-commit-id: c5f2ef462022af435f4694d3ab5450d6a7a89ee9

--- Backend/Accounts/urls.py
+++ Backend/Accounts/urls.py
@@ -8,13 +8,24 @@ urlpatterns = [
     path('login/', obtain_jwt_token),
     path('login/verify/', verify_jwt_token),
     path('login/refresh/', refresh_jwt_token),
+
+    # 중복확인, 일치 확인
     path('check/pin/', views.pin_check),
-    path('check/email/', views.email_check),
     path('check/user/', views.user_check),
+    path('check/email/', views.email_check),
+
+    # 이메일 인증
+    path('send/email/', views.email_code),
+    path('send/email/confirm/', views.email_code_confirm),
+
+    # 회원정보수정 - 핀번호, 비밀번호, 삭제
     path('setting/password/<int:pk>/', views.password_setting),
     path('setting/pincode/<int:pk>/', views.pincode_setting),
     path('setting/delete/<int:pk>/', views.user_delete),
+    # 비밀번호 찾기
     path('find/password/', views.password_find),
+    path('find/password/confirm/', views.password_find_confirm),
+    path('find/password/setting/<int:pk>/', views.password_find_setting),
     # 아이계정
     path('child/', views.child_create_read),
     path('child/setting/', views.child_update),
