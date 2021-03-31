199cfe5dee9fb8e48fd48d7b7cade35b81fdf7b8
명도균 <dorosee97@gmail.com>
2021-03-31 15:47:42 +0900

    [feat/FE] : 튜토리얼 라이브러리 설치
    - 튜토리얼 파일 생성
    - 코드  리팩토링
    
    
    Former-commit-id: df963c72a05af4e6616dca710ba909e6491b63b0 [formerly ce72315fa0d814c940791553bc2c218198231283]
    Former-commit-id: ad1adbc4081ccfdfc3842f391b91fda5ba212fec

--- Frontend/src/components/elements/RoundButton.js
+++ Frontend/src/components/elements/RoundButton.js
@@ -3,17 +3,33 @@ import {Text, StyleSheet, Dimensions, TouchableOpacity} from 'react-native';
 import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
 
 // arrow는 true이면 round 형태의 화살표 버튼, false이면 round 형태의 텍스트 버튼
-function RoundButton({text, arrow, onHandlePress}) {
+function RoundButton({text, arrow, onHandlePress, color, styleProps}) {
   return (
     <TouchableOpacity
       onPress={() =>
         onHandlePress ? onHandlePress() : alert('함수를 props로 내려주세요!')
       }
       activeOpacity={0.7}
-      style={[
-        styles.container,
-        {borderWidth: arrow ? 0 : 6, padding: arrow ? 4 : 10},
-      ]}>
+      style={
+        styleProps
+          ? [
+              styles.container,
+              {
+                borderWidth: arrow ? 0 : 6,
+                padding: arrow ? 4 : 10,
+                backgroundColor: color ? color : '#FB537B',
+              },
+              styleProps,
+            ]
+          : [
+              styles.container,
+              {
+                borderWidth: arrow ? 0 : 6,
+                padding: arrow ? 4 : 10,
+                backgroundColor: color ? color : '#FB537B',
+              },
+            ]
+      }>
       {arrow ? (
         <FontAwesome5Icon
           style={styles.arrowIcon}
