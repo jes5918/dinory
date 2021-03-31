78942e58d3fdd85041faeaf6aa335b5bec891d23
zngsm <68373473+zngsm@users.noreply.github.com>
2021-03-31 12:14:04 +0900

    [fix/BE] : 계정views 리팩토링 및 일부 수정
    - 계정 기능별 views 분리
    - delete 요청시 body 삭제 토큰정보로 확인하기
    - find 비밀번호 수정 POST -> PUT 수정
    
    Resolves : #resolve
    
    
    Former-commit-id: 6b44bd20d994898d4c0d8ebf2869dfe63fd582c4 [formerly 3565e82f6d5febda4e6f57aac92da1824eb219ca]
    Former-commit-id: 9425027a17ea5f497b4ce4183a4db6b94ab94be2

--- Backend/Accounts/urls.py
+++ Backend/Accounts/urls.py
@@ -1,34 +1,36 @@
 from django.urls import path
 from . import views
+from .views import signup_views, check_views, email_confirm_views, setting_user_views, find_password_views, child_views
 
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 urlpatterns = [
     # 관리자 계정
-    path('signup/', views.signup),
+    path('signup/', signup_views.signup),
     path('login/', obtain_jwt_token),
     path('login/verify/', verify_jwt_token),
     path('login/refresh/', refresh_jwt_token),
 
     # 중복확인, 일치 확인
-    path('check/pin/', views.pin_check),
-    path('check/user/', views.user_check),
-    path('check/email/', views.email_check),
+    path('check/pin/', check_views.pin_check),
+    path('check/user/', check_views.user_check),
+    path('check/email/', check_views.email_check),
 
     # 이메일 인증
-    path('send/email/', views.email_code),
-    path('send/email/confirm/', views.email_code_confirm),
+    path('send/email/', email_confirm_views.email_code),
+    path('send/email/confirm/', email_confirm_views.email_code_confirm),
 
     # 회원정보수정 - 핀번호, 비밀번호, 삭제
-    path('setting/password/<int:pk>/', views.password_setting),
-    path('setting/pincode/<int:pk>/', views.pincode_setting),
-    path('setting/delete/<int:pk>/', views.user_delete),
+    path('setting/password/<int:pk>/', setting_user_views.password_setting),
+    path('setting/pincode/<int:pk>/', setting_user_views.pincode_setting),
+    path('setting/delete/<int:pk>/', setting_user_views.user_delete),
+
     # 비밀번호 찾기
-    path('find/password/', views.password_find),
-    path('find/password/confirm/', views.password_find_confirm),
-    path('find/password/setting/<int:pk>/', views.password_find_setting),
+    path('find/password/', find_password_views.password_find),
+    path('find/password/confirm/', find_password_views.password_find_confirm),
+    path('find/password/setting/<int:pk>/', find_password_views.password_find_setting),
     # 아이계정
-    path('child/', views.child_create_read),
-    path('child/setting/', views.child_update),
-    path('child/setting/delete/', views.child_delete),
-    path('child/setting/voice/', views.child_change_voice),
+    path('child/', child_views.child_create_read),
+    path('child/setting/', child_views.child_update),
+    path('child/setting/delete/', child_views.child_delete),
+    path('child/setting/voice/', child_views.child_change_voice),
 ]
