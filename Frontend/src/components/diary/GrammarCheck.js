6addc69a4fe66f3497fe7f0849aa5d60cecc9a89
명도균 <dorosee97@gmail.com>
2021-03-31 15:47:42 +0900

    [feat/FE] : 튜토리얼 라이브러리 설치
    - 튜토리얼 파일 생성
    - 코드  리팩토링
    
    
    Former-commit-id: 9d9de837d859c72286ce342202ca7927d6c7730d

--- Frontend/src/components/diary/GrammarCheck.js
+++ Frontend/src/components/diary/GrammarCheck.js
@@ -1,80 +1,120 @@
-import React, {useEffect} from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
-  KeyboardAvoidingView,
-  StyleSheet,
-  TextInput,
-  Button,
-  TouchableOpacity,
+  Dimensions,
   ImageBackground,
-  ScrollView,
+  StyleSheet,
+  Image,
 } from 'react-native';
+import TypeWriter from 'react-native-typewriter';
 import RoundButton from '../../components/elements/RoundButton';
-import Sound from 'react-native-sound';
 
-const audioURL = 'https://j4b105.p.ssafy.io/media/tts_basic/button-1.mp3';
-// const audioURL = require('../../assets/Wellcometodinory.wav');
+const dimensions = Dimensions.get('window');
+const width = dimensions.width;
+const height = dimensions.height;
 
 export default function GrammarCheck() {
-  let whoosh = new Sound(audioURL, Sound.MAIN_BUNDLE, (error) => {
-    if (error) {
-      console.log('failed to load the sound', error);
-    }
-    console.log('아무거나찍자');
-    // loaded successfully
-    // console.log(
-    //   'duration in seconds: ' +
-    //     whoosh.getDuration() +
-    //     'number of channels: ' +
-    //     whoosh.getNumberOfChannels(),
-    // );
-
-    // // Play the sound with an onEnd callback
-    // whoosh.play((success) => {
-    //   if (success) {
-    //     console.log('successfully finished playing');
-    //   } else {
-    //     console.log('playback failed due to audio decoding errors');
-    //   }
-    // });
-  });
-
-  // let music = new Sound(audioURL, null, (error) => {
-  //   if (error) {
-  //     console.log(error);
-  //   }
-  // });
-  return (
-    <View style={styles.container}>
-      <Button
-        title={'재생'}
-        onPress={() => {
-          whoosh.play();
-        }}
-        name={'controller-play'}
-        size={40}
+  const url = require('../../assets/images/tutorial/IT1.png');
+  if (num === 1) {
+    return (
+      <ImageBackground
+        source={url}
+        style={{width: width, height: height, resizeMode: 'stretch'}}>
+        <View
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            alignSelf: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'flex-end',
+            alignItems: 'flex-end',
+          }}>
+          <RoundButton
+            text={'Next'}
+            color={'#56A4F1'}
+            styleProps={{margin: width * 0.03}}
+          />
+          <View
+            style={{
+              backgroundColor: 'rgba(256, 256, 256, 0.8)',
+              width: width,
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'flex-start',
+              padding: width * 0.01,
+              height: height * 0.25,
+              paddingHorizontal: width * 0.04,
+              zIndex: 15,
+            }}>
+            <TypeWriter
+              style={{
+                fontFamily: 'HoonPinkpungchaR',
+                fontSize: width * 0.026,
+                color: 'black',
+                lineHeight: width * 0.04,
+              }}
+              typing={1}
+              fixed={true}
+              initialDelay={500}
+              minDelay={60}>
+              안녕! 지금부터 같이 사진 일기를 써볼거에요. 먼저, 일기를 쓸 사진을
+              선택해주세요! 사진 로딩이 조금 걸릴 수 있으니 선택 후 조금만
+              기다려주세요!
+            </TypeWriter>
+          </View>
+        </View>
+      </ImageBackground>
+    );
+  } else if (num === 2) {
+    <View
+      style={{
+        position: 'absolute',
+        bottom: 0,
+        alignSelf: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
+      }}>
+      <RoundButton
+        text={'Next'}
+        color={'#56A4F1'}
+        styleProps={{margin: width * 0.03}}
       />
-      <Button
-        title={'일시정지'}
-        onPress={() => {
-          whoosh.pause();
-        }}
-        name={'controller-paus'}
-        size={40}
-      />
-    </View>
-  );
+      <View
+        style={{
+          backgroundColor: 'rgba(256, 256, 256, 0.8)',
+          width: width,
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'flex-start',
+          padding: width * 0.01,
+          // borderRadius: 30,
+          height: height * 0.25,
+          paddingHorizontal: width * 0.04,
+          zIndex: 15,
+          // flexWrap: wrap',
+        }}>
+        <TypeWriter
+          style={{
+            fontFamily: 'HoonPinkpungchaR',
+            fontSize: width * 0.026,
+            color: 'black',
+            lineHeight: width * 0.04,
+          }}
+          typing={1}
+          fixed={true}
+          initialDelay={500}
+          minDelay={60}>
+          안녕! 지금부터 같이 사진 일기를 써볼거에요. 먼저, 일기를 쓸 사진을
+          선택해주세요! 사진 로딩이 조금 걸릴 수 있으니 선택 후 조금만
+          기다려주세요!
+        </TypeWriter>
+      </View>
+    </View>;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    height: 100,
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    backgroundColor: 'lightgrey',
-    opacity: 0.3,
-    flexDirection: 'row',
-  },
-});
