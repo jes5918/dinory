989abd715eae29ade3f2fac23e5b244951dd4ea9
윤지해 <stanajh@naver.com>
2021-04-01 13:28:34 +0900

    [feat/FE] : 스타일 수정 및 로그인페이지로 이동 추가 style 전체 수정 및 로그인 페이지로 이동 추가
    Former-commit-id: 3c1b4d7190663fba19c3aba98e11eea0327196be [formerly 7558019f5e2b2d4774140f5601079a1e175484a5]
    Former-commit-id: 4127989fff39a781d51670e0c459cf6316dd873a

--- Frontend/src/components/authorization/AuthTitle.js
+++ Frontend/src/components/authorization/AuthTitle.js
@@ -1,11 +1,9 @@
-import React, {Component, useState} from 'react';
-import {
-  StyleSheet,
-  ImageBackground,
-  ScrollView,
-  View,
-  Text,
-} from 'react-native';
+import React from 'react';
+import {StyleSheet, View, Text, Dimensions} from 'react-native';
+
+const dimensions = Dimensions.get('window');
+const height = dimensions.height;
+
 export default function AuthTitle({title}) {
   return (
     <View style={styles.container}>
@@ -19,7 +17,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
-    fontSize: 40,
+    fontFamily: 'NotoSansKR-Bold',
+    fontSize: height * 0.06,
     fontWeight: 'bold',
     color: '#707070',
   },
