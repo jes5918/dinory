d506a9583fa2f9b6c93e8554b802dc60d2652258
유진우 <phoenix9373@naver.com>
2021-04-01 14:59:55 +0900

    [feat/FE] : 머지 오류로 인한 코드 재수정
    Resolves : #resolve
    
    
    Former-commit-id: 6b4025db8f1dda4e1da7a24c3b1ee0440cb1f13f

--- Frontend/src/views/parent/ParentSetting.js
+++ Frontend/src/views/parent/ParentSetting.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import {
   View,
-  Image,
   Text,
   StyleSheet,
   TouchableOpacity,
@@ -9,7 +8,9 @@ import {
 } from 'react-native';
 import {useNavigation} from '@react-navigation/core';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {removeChildProfile} from '../../api/accounts/childSettings';
 
+// components
 import BackgroundAbsolute from '../../components/elements/BackgroundAbsolute';
 import Header from '../../components/elements/Header';
 import Layout from '../../components/elements/Layout';
@@ -21,99 +22,189 @@ const url = require('../../assets/images/background2.png');
 const windowSize = Dimensions.get('window');
 const windowWidth = windowSize.width;
 const windowHeight = windowSize.height; // 752
-const AsyncStorageKeys = [
-  'jwt',
-  'email',
-  'username',
-  'password',
-  'password_confirmation',
-  'pin_code',
-  'pin_code_confirmation',
-  'ProfileYear',
-  'ProfileName',
-];
-let presentKeys = [];
+
+// 현재 스토리지에 있는 키
+// ['jwt', 'user_pk', 'profile'];
 
 function ParentSetting() {
-  const [modalVisible, setModalVisible] = useState(false);
+  const [logoutModal, setLogoutModal] = useState(false);
+  const [profileDelete, setProfileDelete] = useState(false);
   const [updateInfoModal, setUpdateInfoModal] = useState(false);
+  const [logoutInfoModal, setLogoutInfoModal] = useState(false);
+  const [profileDeleteInfoModal, setProfileDeleteInfoModal] = useState(false);
   const navigation = useNavigation();
 
-  const getAllKeys = async () => {
-    try {
-      presentKeys = await AsyncStorage.getAllKeys();
-    } catch (e) {
-      console.error('AsyncStorage getAllKeys Fail : ', e);
-    }
+  const executeLogout = async () => {
+    const willRemovedKeys = ['jwt', 'profile'];
 
-    console.log('AsyncStorage getAllKeys Done : ', presentKeys);
-  };
-
-  const removeFew = async () => {
-    const willRemovedKeys = presentKeys.filter((key) =>
-      AsyncStorageKeys.includes(key),
-    );
-    console.log(willRemovedKeys);
     try {
-      setModalVisible(!modalVisible);
+      setLogoutModal(!logoutModal);
       await AsyncStorage.multiRemove(willRemovedKeys);
       navigation.navigate('HomeScreen');
     } catch (e) {
       console.log('AsyncStorage Remove Keys Fail : ', e);
     }
 
-    console.log('AsyncStorage Remove Keys Done');
+    console.log(
+      'AsyncStorage Remove Keys Done(다음 키들을 삭제했습니다.) : ',
+      willRemovedKeys,
+    );
   };
 
+  const deleteProfile = async () => {
+    const willRemovedKeys = ['profile'];
+
+    try {
+      setProfileDelete(!profileDelete);
+      await AsyncStorage.multiRemove(willRemovedKeys);
+      navigation.navigate('SelectProfile'); // 프로필이 아예 없는 경우의 로직 구현 필요
+    } catch (e) {
+      console.log('AsyncStorage Remove Keys Fail : ', e);
+    }
+
+    console.log(
+      'AsyncStorage Remove Keys Done(다음 키들을 삭제했습니다.) : ',
+      willRemovedKeys,
+    );
+  };
+
+  const fetchRemoveProfile = async () => {
+    let profile_pk = '';
+    await AsyncStorage.getItem('profile').then((profile) => {
+      const data = JSON.parse(profile);
+      profile_pk = data.profile_pk;
+    });
+
+    if (profile_pk) {
+      removeChildProfile(
+        profile_pk,
+        (res) => {
+          console.log('프로필이 정상적으로 삭제되었습니다.');
+        },
+        (e) => {
+          console.error('프로필 삭제에서 에러가 발생했습니다. : ', e);
+        },
+      );
+    } else {
+      console.error(
+        '문제: AsyncStorage에서 프로필 ID를 못 불러왔습니다.(ParentSetting.js를 확인해주세요.)',
+      );
+    }
+  };
+
+  // ===========================================>
+  // 프로필 삭제
   const onHandleProfileDelete = () => {
-    alert('프로필을 삭제하시겠습니까?');
+    setProfileDelete(!profileDelete);
   };
 
-  const onHandlePressAllow = async () => {
-    await getAllKeys();
-    await removeFew();
+  const onHandlePressAllowProfileDelete = async () => {
+    await fetchRemoveProfile();
+    await deleteProfile();
   };
 
+  const onHandlePressRefuseProfileDelete = () => {
+    setProfileDelete(!profileDelete);
+  };
+  // <===========================================
+
+  // ===========================================>
+  // 로그아웃
   const onHandleLogout = () => {
-    setModalVisible(!modalVisible);
+    setLogoutModal(!logoutModal);
   };
 
-  const onHandlePressRefuse = () => {
-    setModalVisible(!modalVisible);
+  const onHandlePressAllowLogout = async () => {
+    await executeLogout();
   };
 
-  const changeModalState = () => {
-    setUpdateInfoModal(!updateInfoModal);
+  const onHandlePressRefuseLogout = () => {
+    setLogoutModal(!logoutModal);
   };
+  // <===========================================
 
-  const closeModal = () => {
+  // ===========================================>
+  // 준비중인 기능에 대한 안내.
+  const willUpdateInfomationTimeModal = () => {
     setTimeout(() => {
       setUpdateInfoModal(!updateInfoModal);
     }, 1000);
   };
 
-  const willUpdateInfomation = () => {
+  const willUpdateInfomationCloseModal = () => {
     setUpdateInfoModal(!updateInfoModal);
   };
+  // <===========================================
+
+  // ===========================================>
+  // 로그아웃 완료에 대한 안내.
+  const logoutCompleteInfomationTimeModal = () => {
+    setTimeout(() => {
+      setLogoutInfoModal(!logoutInfoModal);
+    }, 1000);
+  };
+
+  const logoutCompleteInfomationCloseModal = () => {
+    setLogoutInfoModal(!logoutInfoModal);
+  };
+  // <===========================================
+
+  // ===========================================>
+  // 프로필 삭제 완료에 대한 안내.
+  const profileDeleteInfomationTimeModal = () => {
+    setTimeout(() => {
+      setProfileDeleteInfoModal(!profileDeleteInfoModal);
+    }, 1000);
+  };
+
+  const profileDeleteInfomationCloseModal = () => {
+    setProfileDeleteInfoModal(!profileDeleteInfoModal);
+  };
+  // <===========================================
 
   return (
     <View style={styles.container}>
       <SelectModal
-        modalVisible={modalVisible}
+        modalVisible={logoutModal}
         alertText={'정말 로그아웃 하시겠습니까?'}
         refuseText={'취소'}
         allowText={'로그아웃'}
-        onHandlePressAllow={onHandlePressAllow}
-        onHandlePressRefuse={onHandlePressRefuse}
+        onHandlePressAllow={onHandlePressAllowLogout}
+        onHandlePressRefuse={onHandlePressRefuseLogout}
         secondText={'자동 로그인을 한 경우, 해제됩니다.'}
+      />
+      <SelectModal
+        modalVisible={profileDelete}
+        alertText={'해당 프로필을 지우시겠습니까?'}
+        refuseText={'취소'}
+        allowText={'삭제하기'}
+        onHandlePressAllow={onHandlePressAllowProfileDelete}
+        onHandlePressRefuse={onHandlePressRefuseProfileDelete}
+        secondText={'프로필의 모든 데이터가 삭제됩니다.'}
       />
       <AlertModal
         modalVisible={updateInfoModal}
-        onHandleCloseModal={() => changeModalState()}
+        onHandleCloseModal={() => willUpdateInfomationCloseModal()}
         text={'현재 준비중인 기능입니다.'}
         iconName={'warning'}
         color={'#ffcc00'}
-        setTimeFunction={() => closeModal()}
+        setTimeFunction={() => willUpdateInfomationTimeModal()}
+      />
+      <AlertModal
+        modalVisible={logoutInfoModal}
+        onHandleCloseModal={() => logoutCompleteInfomationCloseModal()}
+        text={'로그아웃이 완료되었습니다.'}
+        iconName={'warning'}
+        color={'#ffcc00'}
+        setTimeFunction={() => logoutCompleteInfomationTimeModal()}
+      />
+      <AlertModal
+        modalVisible={profileDeleteInfoModal}
+        onHandleCloseModal={() => profileDeleteInfomationCloseModal()}
+        text={'프로필이 삭제되었습니다.'}
+        iconName={'warning'}
+        color={'#ffcc00'}
+        setTimeFunction={() => profileDeleteInfomationTimeModal()}
       />
       <BackgroundAbsolute style={styles.container} imageSrc={url}>
         <Header />
@@ -123,39 +214,31 @@ function ParentSetting() {
           height={windowHeight * 0.8}
           opacity={1}>
           <TouchableOpacity
-            onPress={() => willUpdateInfomation()}
+            onPress={() => willUpdateInfomationCloseModal()}
             activeOpacity={0.7}
             style={styles.mainButton}>
             <Text style={styles.mainText}>일기 검사하기</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            onPress={() => willUpdateInfomation()}
+            onPress={() => willUpdateInfomationCloseModal()}
             activeOpacity={0.7}
             style={styles.mainButton}>
             <Text style={styles.mainText}>통계 보기</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            onPress={() =>
-              navigation.navigate('PinAuthentication', {
-                connetedRoute: 'PinUpdate',
-              })
-            }
+            onPress={() => navigation.navigate('PinUpdate')}
             activeOpacity={0.7}
             style={styles.mainButton}>
             <Text style={styles.mainText}>핀 번호 변경</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            onPress={() =>
-              navigation.navigate('PinAuthentication', {
-                connetedRoute: 'PassWordUpdate',
-              })
-            }
+            onPress={() => navigation.navigate('PassWordUpdate')}
             activeOpacity={0.7}
             style={styles.mainButton}>
             <Text style={styles.mainText}>비밀번호 변경</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            onPress={() => console.log('clicked')}
+            onPress={() => onHandleProfileDelete()}
             activeOpacity={0.7}
             style={styles.mainButton}>
             <Text style={[styles.mainText, {color: '#FF3120'}]}>
