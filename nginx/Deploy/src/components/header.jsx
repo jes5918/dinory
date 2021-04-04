4e8e4e2e63b2c7f46b5d8b340431cad15e346296
jes5918 <jes5918@koreatech.ac.kr>
2021-04-04 08:10:11 +0000

    Fix/fe api
    Former-commit-id: d3b0cd158fd95623d2974083f975e9f966001927 [formerly 18c482cde62f73f9b4a6a496ac9f9ca568a61dd5]
    Former-commit-id: d055c1451f7645bd4df68cc8c0b7f981e1a1966b
    Former-commit-id: d0aa232d9756ea0af90323a77f0c06f9dd58fbaf

--- nginx/Deploy/src/components/header.jsx
+++ nginx/Deploy/src/components/header.jsx
@@ -1,6 +1,29 @@
 import playStoreBadge from '../assets/google-play-badge.png';
-
+import anime from 'animejs';
+import { useEffect } from 'react';
 export const Header = (props) => {
+  useEffect(() => {
+    var textWrapper = document.querySelector('.ml6 .letters');
+    textWrapper.innerHTML = textWrapper.textContent.replace(
+      /\S/g,
+      "<span class='letter'>$&</span>"
+    );
+  }, []);
+
+  anime.timeline({ loop: true }).add({
+    targets: '.ml6 .letter',
+    translateY: ['1.5em', 0],
+    translateZ: 0,
+    duration: 2000,
+    delay: (el, i) => 100 * i,
+  });
+  // .add({
+  //   targets: '.ml6',
+  //   opacity: 0,
+  //   duration: 1000,
+  //   easing: 'easeOutExpo',
+  //   delay: 1000,
+  // });
   return (
     <header id='header'>
       <div className='intro'>
@@ -8,14 +31,13 @@ export const Header = (props) => {
           <div className='container'>
             <div className='row'>
               <div className='col-md-8 col-md-offset-2 intro-text'>
-                <h1>
+                <h1 class='ml6'>
                   SSAFY 4th PROJECT
-                  <br />
-                  <span>play with Dinory</span>
+                  <span class='text-wrapper'>
+                    <span class='letters'>play with Dinory</span>
+                  </span>
                 </h1>
                 <p>Compatible with Android Devices</p>
-                {/* <br /> */}
-                {/* <p>DAEJEON B105</p> */}
                 <a
                   href='#features'
                   className='btn btn-custom btn-lg page-scroll'
